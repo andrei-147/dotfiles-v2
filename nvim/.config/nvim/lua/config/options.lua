bca1ea0011bf20e82e578aa15a0de20c746ea23b
andrei-147 <u.andrei2010@gmail.com>
2025-09-27 01:59:51 +0300

    configured nvim completely for now

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.autoformat = false
+
+vim.opt.tabstop = 4 -- a real tab character equals 4 spaces
+vim.opt.shiftwidth = 4 -- autoindent uses 4 spaces
+vim.opt.softtabstop = 4 -- pressing <Tab> inserts 4 spaces
+vim.opt.expandtab = true -- convert typed tabs to spaces
+vim.opt.smarttab = true
